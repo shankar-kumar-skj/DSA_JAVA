64b7ce5a59188715a0590398334a9533d7d2735c
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2025-10-14 13:20:25 +0530

    Search_in_sorted_matrix_search_matrix_240

--- LECTURE34/Search_in_sorted_matrix.java
+++ LECTURE34/Search_in_sorted_matrix.java
@@ -28,7 +28,7 @@ public class Search_in_sorted_matrix {
     //     return false;
     // }
 
-//  LEETU PROBLEM 
+//  LEETU PROBLEM 240
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix.length==0){
             return false;
@@ -36,8 +36,18 @@ public class Search_in_sorted_matrix {
         int Rows=matrix.length;
         int Cols=matrix[0].length;
         int r=0;
-        int c=matrix[0].length-1;
-
+        int c=Cols-1;
+        while(r<Rows && c>=0){
+            if(matrix[r][c]==target){
+                return true;
+            }
+            else if(target<matrix[r][c]){
+                c=c-1;
+            }
+            else{
+                r=r+1;
+            }
+        }
         return false;
     }
 }
