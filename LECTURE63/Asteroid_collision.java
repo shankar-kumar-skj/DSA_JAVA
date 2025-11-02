8edfaacef5e3038167762678c50fe13a0ef75cf0
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2025-11-02 19:35:13 +0530

    Asteroid_collision

---
+++ LECTURE63/Asteroid_collision.java
@@ -0,0 +1,48 @@
+package JAVA.SHASHCODE.LECTURE63;
+
+import java.util.Stack;
+
+public class Asteroid_collision {
+    public static void main(String[] args) {
+        
+    }
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack=new Stack<>();
+        for(int i=0;i<asteroids.length;i++){
+            // if stack is empty or positive value arrives then perform blind insertion
+            if(stack.isEmpty()|| asteroids[i]>0){
+                stack.push(asteroids[i]);
+            }
+            else{
+                while(!stack.isEmpty()){
+                    int top =stack.peek();
+                    if(top<0){
+                        stack.push(asteroids[i]);
+                        break;
+                    }
+                    int modVal=Math.abs(asteroids[i]);
+                        if(modVal == top){
+                            stack.pop();
+                            break;
+                        }
+                        else if(modVal<top){
+                            break;
+                        }
+                        else{
+                            stack.pop();
+                            if(stack.isEmpty()){
+                                stack.push(asteroids[i]);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        int len=stack.size();
+        int ansArray[]=new int [len];
+        for(int i=len-1;i>=0;i--){
+            ansArray[i]=stack.pop();
+        }
+        return ansArray;
+    }
+}
