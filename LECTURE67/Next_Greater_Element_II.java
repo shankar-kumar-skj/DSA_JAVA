7e91a4df8d2d7b826f23e60929cd75278aef9125
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2025-11-04 02:25:55 +0530

    Next_Greater_Element_II

---
+++ LECTURE67/Next_Greater_Element_II.java
@@ -0,0 +1,26 @@
+package JAVA.SHASHCODE.LECTURE67;
+
+import java.util.Stack;
+
+public class Next_Greater_Element_II {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> stack=new Stack<>();
+        int n=nums.length;
+        int res[]=new int[n];
+        for(int i=2*n-1;i>=0;i--){
+            while (!stack.isEmpty() && stack.peek()<=nums[i%n]) {
+                stack.pop();
+            }
+            if(i<=n){
+                if(stack.isEmpty()){
+                    res[i]=-1;
+                }
+                else{
+                    res[i]=stack.peek();
+                }
+            }
+            stack.push(nums[i%n]);
+        }
+        return res;
+    }
+}
