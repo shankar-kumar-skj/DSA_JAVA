f8d721c03689c84d5f18d2a55ac2472c3cce65ec
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2025-11-05 01:11:41 +0530

    Longest_valid_parentheses

--- LECTURE70/Longest_valid_parentheses.java
+++ LECTURE70/Longest_valid_parentheses.java
@@ -1,7 +1,50 @@
 package JAVA.SHASHCODE.LECTURE70;
 
+import java.util.Stack;
+
 public class Longest_valid_parentheses {
+    public int longestValidParentheses1(String s) {
+        Stack<Integer> stack=new Stack<>();
+        int max=0;
+        stack.push(-1);
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                stack.push(i); // index
+            }
+            else{
+                stack.pop();
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }
+                else{
+                max=Math.max(max,i-stack.peek());
+                }
+            }
+        }
+        return max;
+    }
+
+    // using custom stack
     public int longestValidParentheses(String s) {
-        
+        Stack<Integer> stack=new Stack<>();
+        int max=0;
+        stack.push(-1);
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                stack.push(i); // index
+            }
+            else{
+                stack.pop();
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }
+                else{
+                max=Math.max(max,i-stack.peek());
+                }
+            }
+        }
+        return max;
     }
 }
