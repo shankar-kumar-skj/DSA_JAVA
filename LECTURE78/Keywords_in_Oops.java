dd887a7b324529f55d352934832098d97975f07b
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2025-11-23 08:36:13 +0530

    OOPs_keywords_in_Java

---
+++ LECTURE78/Keywords_in_Oops.java
@@ -0,0 +1,36 @@
+package JAVA.SHASHCODE.LECTURE78;
+
+
+// this keyword
+// "this" keyword => it refers to instance of current class
+//  "this" keyword used in => attribute,method,static 
+//  "this" keyword not used in => static method
+
+// this() => to call the current class default constructor
+// this(age) => to call the current class age constructor
+// this(obj) => to call the current class copy constructor
+
+
+// Super Keyword
+// "super" keyword => immediate "parent class"
+// super.method_name() => "child class" called "parent class" methods
+// super.variable => "child class" called "parent class" variable
+
+// final keyword
+// final int age =20;
+//       Or
+// final int age;
+// class constructor =>{ this.age = 20 }
+
+// if we want to do this it gives you complie time error
+// final int age =20; 
+// final int age =30; 
+
+// rules:-
+// 1. No recursion calls with this function it make in an infinite loop.
+// 2. "this" & "super" always stand in first line of the class and only one can we used at a time.
+  
+
+public class Keywords_in_Oops {
+    
+}
