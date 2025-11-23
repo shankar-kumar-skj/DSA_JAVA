b86a0863fbd7b7263a5abc0a2a7f078d70fc1c66
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2025-11-23 08:56:20 +0530

    OOPs_Constructor_calling_in_Java

---
+++ LECTURE79/Constructor_calling.java
@@ -0,0 +1,16 @@
+package JAVA.SHASHCODE.LECTURE79;
+
+// when child extends parent class
+//  when child class object is created then 
+// firstly "parent constructor" is called "child constructor"
+
+// RULES :-
+// If the class is accessible?
+// If yes, then check "Access Specifier of method"
+
+// yes, CONSTRUCTOR TO BE PRIVATE
+// => In Case of multiple and make a function not to be changed by other
+
+public class Constructor_calling {
+    
+}
