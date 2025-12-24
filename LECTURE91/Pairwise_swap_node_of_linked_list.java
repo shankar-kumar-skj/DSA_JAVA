8976f29811600665433b3fcb47a8dc296a6c56c9
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2025-12-24 10:55:06 +0530

    Pairwise_swap_node_of_linked_list

---
+++ LECTURE91/Pairwise_swap_node_of_linked_list.java
@@ -0,0 +1,24 @@
+package JAVA.SHASHCODE.LECTURE91;
+
+public class Pairwise_swap_node_of_linked_list {
+    public Node pairwiseSwap(Node head) {
+        Node first=head;
+        Node prev=null;
+        while(first!=null && first.next!=null){
+            Node second =first.next;
+
+            first.next=second.next;
+            second.next=first;
+            // first swap/swapping head node
+            if(prev==null){
+                head=second;
+            }
+            else{
+                prev.next=second;
+            }
+            prev=first;
+            first=first.next;
+        }
+        return head;
+    }
+}
