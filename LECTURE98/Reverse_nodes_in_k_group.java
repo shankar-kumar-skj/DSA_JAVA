f4475c3b118aaafc4ddd78e85da6f9c96ca1e229
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2025-12-26 22:37:00 +0530

    Reverse_nodes_in_k_group

---
+++ LECTURE98/Reverse_nodes_in_k_group.java
@@ -0,0 +1,62 @@
+package JAVA.SHASHCODE.LECTURE98;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Reverse_nodes_in_k_group {
+    public int len(ListNode head) {
+        int counter = 0;
+        while (head != null) {
+            counter++;
+            head = head.next;
+        }
+        return counter;
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        int N = len(head);
+        int groups = N / k;
+        ListNode prevHead = null;
+        ListNode currHead = head;
+        ListNode ansNode = null;
+        for (int i = 0; i < groups; i++) {
+            ListNode prev = null;
+            ListNode cur = currHead;
+            ListNode nextNode = null;
+
+            // reverse k Groups
+            for (int j = 0; j < k; j++) {
+                nextNode = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = nextNode;
+            }
+            if (prevHead == null) {
+                ansNode = prev;
+            } else {
+                prevHead.next = prev;
+            }
+            prevHead = currHead;
+            currHead = cur;
+        }
+        
+        // link the last incomplete group and if last was complete currHead would be
+        // null
+        prevHead.next = currHead;
+        return ansNode;
+    }
+}
