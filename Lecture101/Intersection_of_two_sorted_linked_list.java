9188871b55831b69320d40dd89f76ff124fb78b4
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2025-12-27 12:10:14 +0530

    Intersection_of_two_sorted_linked_list

---
+++ Lecture101/Intersection_of_two_sorted_linked_list.java
@@ -0,0 +1,66 @@
+package JAVA.SHASHCODE.Lecture101;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class Intersection_of_two_sorted_linked_list {
+    public static Node findIntersection(Node head1, Node head2) {
+    // code here.
+        // Node t1=head1;
+        // Node t2=head2;
+        // Node resHead=null;
+        // Node resTail=null;
+        // while(t1!=null && t2!=null){
+        //     if(t1.data==t2.data){
+        //         if(resHead == null){
+        //             resHead=new Node(t1.data);
+        //             resTail=resHead;
+        //         }
+        //         else{
+        //             resTail.next=new Node(t1.data);
+        //             resTail=resTail.next;
+        //         }
+        //         t1=t1.next;
+        //         t2=t2.next;
+        //     }
+        //     else if(t1.data <t2.data){
+        //         t1=t1.next;
+        //     }
+        //     else{
+        //         t2=t2.next;
+        //     }
+        // }
+        // return resHead;
+        
+        //  // dummy head logic
+        Node t1=head1;
+        Node t2=head2;
+        Node resHead=new Node(-1);
+        Node resTail=resHead;
+        while(t1!=null && t2!=null){
+            if(t1.data==t2.data){
+                resTail.next=new Node(t1.data);
+                resTail=resTail.next;
+                t1=t1.next;
+                t2=t2.next;
+            }
+            else if(t1.data <t2.data){
+                t1=t1.next;
+            }
+            else{
+                t2=t2.next;
+            }
+        }
+        Node temp=resHead;
+        resHead=resHead.next;
+        temp.next = null;
+        return resHead;
+    }
+}
