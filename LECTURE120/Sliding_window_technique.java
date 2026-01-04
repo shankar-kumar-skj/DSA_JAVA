0e92f3dd6979b4e56e55537bc1a2990a7a20a207
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-01-04 09:29:16 +0530

    Sliding_window_technique

---
+++ LECTURE120/Sliding_window_technique.java
@@ -0,0 +1,21 @@
+package JAVA.SHASHCODE.LECTURE120;
+
+// Sliding Window
+// 1. Fixed Size
+// 2. Variable Size
+//     (a) Expansion => Adding into the window.
+//     (b) Shrinking => Deleting from the window.
+
+// STEPS OF SLIDING WINDOW :-
+//  1. Initialize the window (not for Variable Size Sliding Window)
+//  2. solve for other elements  means slide the window according to the problem
+//  3. calculate the answer 
+
+public class Sliding_window_technique {
+    public static void main(String[] args) {
+        
+    }
+
+
+
+}
