a684611dfb9770fb4138c22970a0d058e739f8a7
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-01-04 20:11:44 +0530

    Sliding_window_maximum

---
+++ LECTURE129/Sliding_window_maximum.java
@@ -0,0 +1,45 @@
+package JAVA.SHASHCODE.LECTURE129;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// TC => 0(2N) ~ 0(N)
+// SC => O(k) 
+
+public class Sliding_window_maximum {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n=nums.length;
+        int res[]=new int[n-k+1];
+        if(n==0){
+            return res;
+        }
+        Deque<Integer> deque=new ArrayDeque<Integer>();
+        int index=0;
+        while (index<k) {
+            while (!deque.isEmpty() && nums[deque.peekLast()]<=nums[index]) {
+                deque.pollLast();
+            }
+            deque.offerLast(index);
+            index++;
+        }
+
+        res[0] = nums[deque.peekFirst()];
+        // i=1
+        // i-1 <=
+        // i+k-1
+        // 0,1,2
+        // 1,2,3
+        for(int i=1;i<n-k+1;i++){
+            if(!deque.isEmpty() && deque.peekFirst()<=(i-1)){
+                deque.pollFirst();
+            }
+            while (!deque.isEmpty() && nums[deque.peekLast()]<=nums[i+k-1]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i+k-1);
+            res[i] = nums[deque.peekFirst()];
+        }
+        return res;
+    }
+}
