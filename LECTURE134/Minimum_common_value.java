2b35d10103de9d7925fe9e88d01769db5cb11c0f
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-01-05 14:37:29 +0530

    Minimum_common_value

---
+++ LECTURE134/Minimum_common_value.java
@@ -0,0 +1,22 @@
+package JAVA.SHASHCODE.LECTURE134;
+
+public class Minimum_common_value {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i=0;
+        int j=0;
+        int n=nums1.length;
+        int m=nums2.length;
+        while(i<n && j<m){
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
+            }
+            if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        return -1;
+    }
+}
