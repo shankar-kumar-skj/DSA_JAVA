2cf0dbee62b456cf90ee3f1990e96b016a8c9e75
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-01-05 14:03:20 +0530

    Maximum_odd_binary_number

---
+++ LECTURE131/Maximum_odd_binary_number.java
@@ -0,0 +1,25 @@
+package JAVA.SHASHCODE.LECTURE131;
+
+public class Maximum_odd_binary_number {
+    public String maximumOddBinaryNumber(String s) {
+        int n=s.length();
+        int left=0;
+        int right=n-1;
+        char arr[]=s.toCharArray();
+        while(left<=right){
+            if(arr[left]=='1'){
+                left++;
+            }
+            if(arr[right]=='0'){
+                right--;
+            }
+            if(left<right && arr[left]=='0' && arr[right]=='1'){
+                arr[left]='1';
+                arr[right]='0';
+            }
+        }
+        arr[right]='0';
+        arr[n-1]='1';
+        return new String(arr);
+    }
+}
