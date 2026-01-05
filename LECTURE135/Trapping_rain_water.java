df9d982473703f503ad5c2d5ec7f622f2bb77527
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-01-05 14:56:58 +0530

    Trapping_rain_water

---
+++ LECTURE135/Trapping_rain_water.java
@@ -0,0 +1,29 @@
+package JAVA.SHASHCODE.LECTURE135;
+
+public class Trapping_rain_water {
+    public int trap(int[] height) {
+        int n=height.length;
+        int  leftMax=0;
+        int rightMax=0;
+        int l=0;
+        int r=n-1;
+        int ans=0;
+        while(l<r){
+            if(height[l]>leftMax){
+                leftMax=Math.max(leftMax,height[l]);
+            }
+            if(height[r]>rightMax){
+                rightMax=Math.max(rightMax,height[r]);
+            }
+            if(leftMax<rightMax){
+                ans=ans+leftMax-height[l];
+                l++;
+            }
+            else{
+                ans=ans+rightMax-height[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+}
