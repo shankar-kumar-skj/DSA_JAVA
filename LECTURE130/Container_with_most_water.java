e11c407fb5a39d65d6d5a990fd42746478d7e51a
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-01-05 13:49:18 +0530

    Container_with_most_water

---
+++ LECTURE130/Container_with_most_water.java
@@ -0,0 +1,19 @@
+package JAVA.SHASHCODE.LECTURE130;
+
+public class Container_with_most_water {
+    public int maxArea(int[] height) {
+        int i=0;
+        int j=height.length-1;
+        int ans=0;
+        while(i<j){
+            ans=Math.max(ans,(j-i)*Math.min(height[i],height[j]));
+            if(height[i]<=height[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return ans;
+    }
+}
