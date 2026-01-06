d91f9485d4b4e452e521a6a0606fe419f677b9b1
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-01-06 19:40:57 +0530

    Move_pieces_to_obtain_a_string

---
+++ LECTURE144/Move_pieces_to_obtain_a_string.java
@@ -0,0 +1,36 @@
+package JAVA.SHASHCODE.LECTURE144;
+
+public class Move_pieces_to_obtain_a_string {
+    public boolean canChange(String start, String target) {
+        int i=0;
+        int j=0;
+        int n=start.length();
+        while(i<n || j<n){
+
+            // skip all blanks in start
+            while(i<n && start.charAt(i)=='_'){
+                i++;
+            }
+
+            // skip all blanks in target
+            while(j<n && target.charAt(j)=='_'){
+                j++;
+            }
+
+            // count is same only if both the string end at the same time 
+            if(i==n || j==n){
+                return (i==n && j==n);
+            }
+
+            // check false scenaries
+            if(start.charAt(i)!=target.charAt(j)|| (start.charAt(i)=='L' && j>i) || (start.charAt(i)=='R' && j<i)){
+                return false;
+            }
+
+            // move to the next character after comparision
+            i++;
+            j++;
+        }
+        return true;
+    }
+}
