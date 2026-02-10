5656231d79a2a912ce4f975cf69bea39dc1b6b7b
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-02-10 17:59:44 +0530

    Min_cost_climbing_stairs

---
+++ Dynamic_Programming/Min_cost_climbing_stairs.java
@@ -0,0 +1,57 @@
+package JAVA.SHASHCODE.Dynamic_Programming;
+
+// import java.util.Arrays;
+
+public class Min_cost_climbing_stairs {
+    // public int minCostClimbingStairs(int[] cost) {
+    //     int n=cost.length;
+    //     return recur(n, cost);
+    // }
+    // public int recur(int n, int cost[]){
+    //     // base case
+    //     if(n==0 || n==1){
+    //         return 0;
+    //     }
+    //     int oneStep=cost[n-1]+recur(n-1, cost);
+    //     int twoStep=cost[n-2]+recur(n-2, cost);
+    //     return Math.min(oneStep,twoStep);
+    // }
+
+
+// // DP top down approach
+//     public int minCostClimbingStairs(int[] cost) {
+//         int n=cost.length;
+//         int dp[] = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return recur(n, cost,dp);
+//     }
+//     public int recur(int n, int cost[],int dp []){
+//         // base case
+//         if(n==0 || n==1){
+//             dp[n]=0;
+//             return 0;
+//         }
+//         if(dp[n]!=-1){
+//             return dp[n];
+//         }
+//         int oneStep=cost[n-1]+recur(n-1, cost,dp);
+//         int twoStep=cost[n-2]+recur(n-2, cost,dp);
+//         dp[n]=Math.min(oneStep,twoStep);
+//         return dp[n];
+//     }
+
+
+// // DP Bottom Up approach
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int dp[] = new int[n+1];
+        dp[0]=0;
+        dp[1]=0;
+        for(int state=2;state<=n;state++){
+        int oneStep=cost[state-1]+dp[state-1];
+        int twoStep=cost[state-2]+dp[state-2];
+        dp[state]=Math.min(oneStep,twoStep);
+        }
+        return dp[n];
+    }
+}
