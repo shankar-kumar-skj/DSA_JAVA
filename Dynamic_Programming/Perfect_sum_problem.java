a94c7652f49c77e6bd0e9c0726b90d4f868964b7
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-02-15 16:52:05 +0530

    perfectSum

---
+++ Dynamic_Programming/Perfect_sum_problem.java
@@ -0,0 +1,123 @@
+package JAVA.SHASHCODE.Dynamic_Programming;
+
+public class Perfect_sum_problem {
+    // // array with non negative integer
+    // public int perfectSum(int[] nums, int target) {
+    // // code here
+    // int n=nums.length;
+    // return recur(nums, target, n-1);
+
+    // }
+    // int recur(int nums[],int target,int index){
+    // if(index==0){
+    // if(target ==0 && nums[index] ==0){
+    // return 2;
+    // }
+    // else if(target == 0) return 1;
+    // else if(target == nums[index]) return 1;
+    // else return 0;
+    // }
+    // int pick =0;
+    // if(nums[index]<=target){
+    // pick=recur(nums,target-nums[index],index-1);
+    // }
+    // int noPick=recur(nums,target,index-1);
+
+    // return pick+noPick;
+    // }
+
+    // // whole positive no zero involved!
+    // int ZeroRecur(int nums[],int target,int index){
+    // if(target==0) return 1;
+    // if(index==0){
+    // if(target == nums[index]) return 1;
+    // else return 0;
+    // }
+    // int pick =0;
+    // if(nums[index]<=target){
+    // pick=recur(nums,target-nums[index],index-1);
+    // }
+    // int noPick=recur(nums,target,index-1);
+
+    // return pick+noPick;
+    // }
+
+    // // DP => Memorization
+    // public int perfectSum(int[] nums, int target) {
+    // // code here
+    // int n=nums.length;
+    // int dp[][] = new int[n][target+1];
+    // for(int i=0;i<n;i++){
+    // for(int j=0;j<target+1;j++){
+    // dp[i][j]=-1;
+    // }
+    // }
+    // return recur(nums, target, n-1,dp);
+    // }
+
+    // int recur(int nums[],int target,int index,int dp[][]){
+    // if(index==0){
+    // if(target ==0 && nums[index] ==0){
+    // dp[index][target]=2;
+    // return 2;
+    // }
+    // else if(target == 0){
+    // dp[index][target]=1;
+    // return 1;
+    // }
+
+    // else if(target == nums[index]){
+    // dp[index][target]=1;
+    // return 1;
+    // }
+    // else {
+    // dp[index][target]=0;
+    // return 0;
+    // }
+    // }
+
+    // if(dp[index][target]!=-1){
+    // return dp[index][target];
+    // }
+    // int pick =0;
+    // if(nums[index]<=target){
+    // pick=recur(nums,target-nums[index],index-1,dp);
+    // }
+    // int noPick=recur(nums,target,index-1,dp);
+    // dp[index][target] = pick+noPick;
+    // return dp[index][target];
+    // }
+
+    // DP => Tabulation (Bootom Up Approach)
+    public int perfectSum(int[] nums, int target) {
+        // code here
+        int n = nums.length;
+        int dp[][] = new int[n][target + 1];
+
+        for (int t = 0; t < target + 1; t++) {
+            if (t == 0 && nums[0] == 0) {
+                dp[0][t] = 2;
+            } else if (t == 0) {
+                dp[0][t] = 1;
+            }
+
+            else if (t == nums[0]) {
+                dp[0][t] = 1;
+            } else {
+                dp[0][t] = 0;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < target + 1; j++) {
+                int pick = 0;
+                if (nums[i] <= j) {
+                    pick = dp[i-1][j - nums[i]];
+                }
+                int noPick = dp[i-1][j];
+                dp[i][j] = pick + noPick;
+            }
+        }
+        return dp[n-1][target];
+    }
+}
