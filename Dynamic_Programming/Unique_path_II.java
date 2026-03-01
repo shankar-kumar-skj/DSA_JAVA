e037e9c8b74f1106e175ba221483b6d676a882f5
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-03-01 16:27:36 +0530

    Unique_path_II

---
+++ Dynamic_Programming/Unique_path_II.java
@@ -0,0 +1,28 @@
+package JAVA.SHASHCODE.Dynamic_Programming;
+
+public class Unique_path_II {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        return uniquePaths(obstacleGrid,m, n);
+    }
+    public int uniquePaths(int obstacleGrid[][],int m, int n) {
+        int prev[]= new int [n+1];
+        for(int i=1;i<m+1;i++){
+            int cur[]= new int [n+1];
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0 || obstacleGrid[i-1][j-1]==1){ // base case
+                    cur[j]=0;
+                }
+                else if(i==1 && j==1){
+                    cur[j]=1;
+                }
+                else{
+                    cur[j]=prev[j]+cur[j-1];
+                }
+            }
+            prev=cur;
+        }
+        return prev[n];
+    }
+}
