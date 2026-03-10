b0509687cc495de596dd250d2b6b0c834f95759b
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-03-11 05:23:41 +0530

    Sum_of_binary_tree

---
+++ LECTURE174/Sum_of_binary_tree.java
@@ -0,0 +1,23 @@
+package JAVA.SHASHCODE.LECTURE174;
+
+import java.util.*;
+class Node{
+    int data;
+    Node left,right;
+    Node (int d){
+        data=d;
+        left=right=null;
+    }
+}
+
+public class Sum_of_binary_tree {
+    static int sumBT(Node head){
+        if(head==null){
+            return 0;
+        }
+        int leftSum=sumBT(head.left);
+        int rightSum=sumBT(head.right);
+        return (leftSum+rightSum+head.data);
+
+    }
+}
