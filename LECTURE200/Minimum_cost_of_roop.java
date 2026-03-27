7650682675cda76e2bb5ca75237a563a52bcf8f6
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-03-27 22:16:10 +0530

    Minimum_cost_of_roop_priority_queue

---
+++ LECTURE200/Minimum_cost_of_roop.java
@@ -0,0 +1,21 @@
+package JAVA.SHASHCODE.LECTURE200;
+
+import java.util.PriorityQueue;
+
+public class Minimum_cost_of_roop {
+    public static int minCost(int[] arr){
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int element : arr){
+            minHeap.offer(element);
+        }
+        int cost=0;
+        while(minHeap.size()>1){
+            int rope1 = minHeap.poll();
+            int rope2 = minHeap.poll();
+            int newRope=rope1+rope2;
+            cost+=newRope;
+            minHeap.offer(newRope);
+        }
+        return cost;
+    }
+}
