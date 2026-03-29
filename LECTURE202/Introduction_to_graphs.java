230890d9d449989f4eebdde2864786bdf4ac1fe4
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-03-29 09:47:41 +0530

    Introduction_to_graphs

---
+++ LECTURE202/Introduction_to_graphs.java
@@ -0,0 +1,36 @@
+package JAVA.SHASHCODE.LECTURE202;
+
+// GRAPH => NON LINEAR
+// Graph Data Structure is a collection of nodes connected by edges.
+
+// Node/Vertices: Vertices are the fundamental units of the graph. Sometimes, vertices are also known as vertex or nodes. Every node/vertex can be labeled or unlabeled.
+
+// Edges/arcs: Edges are draw or used to connect two nodes of the graph. Edges can connect any two nodes. Sometimes, edges are also known as arcs. Every edge can be labelled/unlabelled.
+
+// Componenents
+//  A — B — C      D — E
+//  Then:
+// {A, B, C} is one component
+// {D, E} is another component
+
+// Directed Graph 
+// A -> B -> C
+
+// UnDirected Graph /Bi-Directed Graph
+// A - B - C
+
+// IN UNDIRECTED GRAPH :
+// Degree => no. of vertices connected with that vertices is its respective degree.
+
+// IN DIRECTED GRAPH :
+// InDegree => no. of edges coming inside on that vertices is its respective Indegree.
+
+// OutDegree => no. of edges moving outside on that vertices is its respective Outdegree.
+
+// Weighted Graph => edge contains weight
+// UnWeighted Graph => edge does n't contains weight
+// 
+
+public class Introduction_to_graphs {
+    
+}
