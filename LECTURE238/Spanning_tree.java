ab94167f7dee46d200562113d97750cd605440a1
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-04-29 10:53:50 +0530

    Spanning_tree\

---
+++ LECTURE238/Spanning_tree.java
@@ -0,0 +1,12 @@
+package JAVA.SHASHCODE.LECTURE238;
+
+// Spanning Tree
+// => N node
+// => N-1 Edges
+// => Undirected Graph
+// => No cycle
+// => Single Component
+
+public class Spanning_tree {
+    
+}
