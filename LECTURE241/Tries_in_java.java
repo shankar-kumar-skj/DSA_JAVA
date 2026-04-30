dca546c46e045837ad1afb735ae8d47d9c2284d2
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-05-01 01:06:24 +0530

    Tries_in_java

---
+++ LECTURE241/Tries_in_java.java
@@ -0,0 +1,78 @@
+package JAVA.SHASHCODE.LECTURE241;
+
+public class Tries_in_java {
+    
+}
+
+class TrieNode{
+
+    TrieNode trieNode[];
+    boolean wordEnd;
+
+    TrieNode(){
+        // c -> a - a => 2
+        trieNode = new TrieNode[26];
+        wordEnd=false;
+    }
+
+    boolean containsChar(char ch){
+        return trieNode[ch-'a']!=null;
+    }
+
+    void add(char ch){
+        trieNode[ch-'a'] = new TrieNode();
+    }
+
+    TrieNode getChar(char ch){
+        return trieNode[ch - 'a'];
+    }
+
+    void markWordEnd(){
+        wordEnd = true;
+    }
+
+    boolean isWord(){
+        return wordEnd;
+    }
+}
+
+class Trie {
+    TrieNode root;
+
+    public Trie() {
+        root=new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode temp = root;
+        for(char ch : word.toCharArray()){
+            if(!temp.containsChar(ch)){
+                temp.add(ch);
+            }
+            temp = temp.getChar(ch);
+        }
+        temp.markWordEnd();     
+    }
+    
+    public boolean search(String word) {
+        TrieNode temp = root;
+        for(char ch : word.toCharArray()){
+            if(!temp.containsChar(ch)){
+                return false;
+            }
+            temp = temp.getChar(ch);
+        }
+        return temp.isWord();     
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode temp = root;
+        for(char ch : prefix.toCharArray()){
+            if(!temp.containsChar(ch)){
+                return false;
+            }
+            temp = temp.getChar(ch);
+        }
+        return temp!=null;     
+    }
+}
