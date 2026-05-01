5be0a33de4ce1b7ba3691f47655817170cc15e65
SHANKAR KUMAR <153372713+shankar-kumar-skj@users.noreply.github.com>
2026-05-01 10:30:36 +0530

    Count_distinct_substring_using_trie_in_java

---
+++ LECTURE244/Count_distinct_substring_using_trie_in_java.java
@@ -0,0 +1,42 @@
+package JAVA.SHASHCODE.LECTURE244;
+
+// TC => O(N * N)
+
+class TrieNode{
+    TrieNode trieNode[];
+    TrieNode(){
+        trieNode = new TrieNode[26];
+    }
+
+    boolean containsChar(char ch){
+        return trieNode[ch-'a']!=null;
+    }
+
+    void add(char ch){
+        trieNode[ch -'a'] = new TrieNode();
+    }
+
+    TrieNode getChar(char ch){
+        return trieNode[ch -'a'];
+    }
+}
+
+public class Count_distinct_substring_using_trie_in_java {
+    public int countDistinct(String s){
+        TrieNode root=new TrieNode();
+        int n=s.length();
+        int count = 0;
+        for(int i=0;i<n;i++){
+            TrieNode temp = root;
+            for(int j=i;j<n;j++){
+                char ch = s.charAt(j);
+                if(!temp.containsChar(ch)){
+                    temp.add(ch);
+                    count++;
+                }
+                temp = temp.getChar(ch);
+            }
+        }
+        return count;
+    }
+}
